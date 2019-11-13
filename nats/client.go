094b1b0c11bd907c2b2c9a4bc3cd06522cecda6d
Hung Nguyen <hung@silentium.io>
2019-11-13 16:59:07 +0700

    removed unused code

--- nats/client.go
+++ nats/client.go
@@ -1,9 +1,5 @@
 package nats
 
-import (
-	"fmt"
-)
-
 type Client struct {
 	Addr string
 }
@@ -14,7 +10,6 @@ func (srv *Client) Request(rq *Request) (*Response, error) {
 		return nil, err
 	}
 	defer func(c *Connection) {
-		fmt.Print("close connection .......")
 		c.Conn.Close()
 	}(c)
 
@@ -24,51 +19,3 @@ func (srv *Client) Request(rq *Request) (*Response, error) {
 func NewClient(addr string) *Client {
 	return &Client{addr}
 }
-
-//
-//func Get(addr string, subject string, path string) (*Response, error) {
-//	return SendJsonRequest(addr, subject, path, nil, nil, "GET")
-//}
-//
-//func Post(addr string, subject string, path string, body *interface{}) (*Response, error) {
-//	return SendJsonRequest(addr, subject, path, body, nil, "POST")
-//}
-//
-//func Put(addr string, subject string, path string, body *interface{}) (*Response, error) {
-//	return SendJsonRequest(addr, subject, path, body, nil, "PUT")
-//}
-//
-//func Delete(addr string, subject string, path string, body *interface{}) (*Response, error) {
-//	return SendJsonRequest(addr, subject, path, body, nil, "PUT")
-//}
-//func Head(addr string, subject string, path string) (*Response, error) {
-//	return SendJsonRequest(addr, subject, path, nil, nil, "HEAD")
-//}
-//func Trace(addr string, subject string, path string, body *interface{}) (*Response, error) {
-//	return SendJsonRequest(addr, subject, path, body, nil, "TRACE")
-//}
-//func Patch(addr string, subject string, path string, body *interface{}) (*Response, error) {
-//	return SendJsonRequest(addr, subject, path, body, nil, "PATCH")
-//}
-//
-//func SendJsonRequest(addr string, subject string, path string, body interface{}, header http.Header, method string) (*Response, error) {
-//	client := &Client{Addr: addr}
-//	jBody, err := bodyToJson(body)
-//	if err != nil {
-//		return nil, err
-//	}
-//	var h http.Header
-//	h = header
-//	if h == nil {
-//		h = http.Header{}
-//	}
-//	rq := &Request{URL: path, Method: method, Headers: h, Body: jBody}
-//	return client.Request(rq)
-//}
-//
-//func bodyToJson(body interface{}) ([]byte, error) {
-//	if body != nil {
-//		return json.Marshal(body)
-//	}
-//	return nil, nil
-//}
--- nats/request_builder.go
+++ nats/request_builder.go
@@ -34,140 +34,140 @@ func NewRequestBuilder() *RequestBuilder {
 // Method
 
 // Head sets the Request method to HEAD and sets the given pathURL.
-func (s *RequestBuilder) Head(pathURL string) *RequestBuilder {
-	s.method = "HEAD"
-	return s.Url(pathURL)
+func (r *RequestBuilder) Head(pathURL string) *RequestBuilder {
+	r.method = "HEAD"
+	return r.Url(pathURL)
 }
 
 // Get sets the Request method to GET and sets the given pathURL.
-func (s *RequestBuilder) Get(pathURL string) *RequestBuilder {
-	s.method = "GET"
-	return s.Url(pathURL)
+func (r *RequestBuilder) Get(pathURL string) *RequestBuilder {
+	r.method = "GET"
+	return r.Url(pathURL)
 }
 
 // Post sets the Request method to POST and sets the given pathURL.
-func (s *RequestBuilder) Post(pathURL string) *RequestBuilder {
-	s.method = "POST"
-	return s.Url(pathURL)
+func (r *RequestBuilder) Post(pathURL string) *RequestBuilder {
+	r.method = "POST"
+	return r.Url(pathURL)
 }
 
 // Put sets the Request method to PUT and sets the given pathURL.
-func (s *RequestBuilder) Put(pathURL string) *RequestBuilder {
-	s.method = "PUT"
-	return s.Url(pathURL)
+func (r *RequestBuilder) Put(pathURL string) *RequestBuilder {
+	r.method = "PUT"
+	return r.Url(pathURL)
 }
 
 // Patch sets the Request method to PATCH and sets the given pathURL.
-func (s *RequestBuilder) Patch(pathURL string) *RequestBuilder {
-	s.method = "PATCH"
-	return s.Url(pathURL)
+func (r *RequestBuilder) Patch(pathURL string) *RequestBuilder {
+	r.method = "PATCH"
+	return r.Url(pathURL)
 }
 
 // Delete sets the Request method to DELETE and sets the given pathURL.
-func (s *RequestBuilder) Delete(pathURL string) *RequestBuilder {
-	s.method = "DELETE"
-	return s.Url(pathURL)
+func (r *RequestBuilder) Delete(pathURL string) *RequestBuilder {
+	r.method = "DELETE"
+	return r.Url(pathURL)
 }
 
 // Options sets the Request method to OPTIONS and sets the given pathURL.
-func (s *RequestBuilder) Options(pathURL string) *RequestBuilder {
-	s.method = "OPTIONS"
-	return s.Url(pathURL)
+func (r *RequestBuilder) Options(pathURL string) *RequestBuilder {
+	r.method = "OPTIONS"
+	return r.Url(pathURL)
 }
 
 // Trace sets the Request method to TRACE and sets the given pathURL.
-func (s *RequestBuilder) Trace(pathURL string) *RequestBuilder {
-	s.method = "TRACE"
-	return s.Url(pathURL)
+func (r *RequestBuilder) Trace(pathURL string) *RequestBuilder {
+	r.method = "TRACE"
+	return r.Url(pathURL)
 }
 
 // Connect sets the Request method to CONNECT and sets the given pathURL.
-func (s *RequestBuilder) Connect(pathURL string) *RequestBuilder {
-	s.method = "CONNECT"
-	return s.Url(pathURL)
+func (r *RequestBuilder) Connect(pathURL string) *RequestBuilder {
+	r.method = "CONNECT"
+	return r.Url(pathURL)
 }
 
 // Url extends the rawURL with the given path by resolving the reference to
 // an absolute URL. If parsing errors occur, the rawURL is left unmodified.
-func (s *RequestBuilder) Url(url string) *RequestBuilder {
-	s.rawURL = url
-	return s
+func (r *RequestBuilder) Url(url string) *RequestBuilder {
+	r.rawURL = url
+	return r
 }
 
 // Header
 
 // Add adds the key, value pair in Headers, appending values for existing keys
 // to the key's values. Header keys are canonicalized.
-func (s *RequestBuilder) AddHeader(key, value string) *RequestBuilder {
-	s.headers.Add(key, value)
-	return s
+func (r *RequestBuilder) AddHeader(key, value string) *RequestBuilder {
+	r.headers.Add(key, value)
+	return r
 }
 
 // Set sets the key, value pair in Headers, replacing existing values
 // associated with key. Header keys are canonicalized.
-func (s *RequestBuilder) SetHeader(key, value string) *RequestBuilder {
-	s.headers.Set(key, value)
-	return s
+func (r *RequestBuilder) SetHeader(key, value string) *RequestBuilder {
+	r.headers.Set(key, value)
+	return r
 }
 
 func (r *RequestBuilder) SetContentType(contentType string) {
 	r.SetHeader(contentType, contentType)
 }
 
-func (s *RequestBuilder) Body(body []byte) *RequestBuilder {
+func (r *RequestBuilder) Body(body []byte) *RequestBuilder {
 	if body == nil {
-		return s
+		return r
 	}
-	return s.BodyProvider(byteBodyProvider{body: body})
+	return r.BodyProvider(byteBodyProvider{body: body})
 }
 
 // BodyProvider sets the RequestBuilder's body provider.
-func (s *RequestBuilder) BodyProvider(body BodyProvider) *RequestBuilder {
+func (r *RequestBuilder) BodyProvider(body BodyProvider) *RequestBuilder {
 	if body == nil {
-		return s
+		return r
 	}
-	s.bodyProvider = body
+	r.bodyProvider = body
 
 	ct := body.ContentType()
 	if ct != "" {
-		s.SetHeader(contentType, ct)
+		r.SetHeader(contentType, ct)
 	}
 
-	return s
+	return r
 }
 
-func (s *RequestBuilder) BodyJSON(bodyJSON interface{}) *RequestBuilder {
+func (r *RequestBuilder) BodyJSON(bodyJSON interface{}) *RequestBuilder {
 	if bodyJSON == nil {
-		return s
+		return r
 	}
-	return s.BodyProvider(jsonBodyProvider{payload: bodyJSON})
+	return r.BodyProvider(jsonBodyProvider{payload: bodyJSON})
 }
 
-func (s *RequestBuilder) AddHeaders(header http.Header) *RequestBuilder {
+func (r *RequestBuilder) AddHeaders(header http.Header) *RequestBuilder {
 	for key, values := range header {
 		for _, value := range values {
-			s.AddHeader(key, value)
+			r.AddHeader(key, value)
 		}
 	}
-	return s
+	return r
 }
 
-func (s *RequestBuilder) Subject(subject string) *RequestBuilder {
-	s.subject = subject
-	return s
+func (r *RequestBuilder) Subject(subject string) *RequestBuilder {
+	r.subject = subject
+	return r
 }
 
-func (s *RequestBuilder) Build() (*Request, error) {
-	_, err := url.Parse(s.rawURL)
+func (r *RequestBuilder) Build() (*Request, error) {
+	_, err := url.Parse(r.rawURL)
 	if err != nil {
-		return nil, errors.New("invalid url " + s.rawURL)
+		return nil, errors.New("invalid url " + r.rawURL)
 	}
 	var body []byte
-	if s.bodyProvider != nil {
-		body, err = s.bodyProvider.Body()
+	if r.bodyProvider != nil {
+		body, err = r.bodyProvider.Body()
 		if err != nil {
 			return nil, errors.New("Invalid body  " + err.Error())
 		}
 	}
-	return &Request{URL: s.rawURL, Method: s.method, Headers: s.headers, Body: body, Subject: s.subject}, nil
+	return &Request{URL: r.rawURL, Method: r.method, Headers: r.headers, Body: body, Subject: r.subject}, nil
 }
