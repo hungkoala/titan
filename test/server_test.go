8f5e6c4f7049666fe0a56847139751c215e31bcb
Hung Nguyen <hung@silentium.io>
2019-11-18 19:44:18 +0700

    fixed blocking nats server

--- test/server_test.go
+++ test/server_test.go
@@ -2,7 +2,9 @@ package test
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -33,7 +35,8 @@ func TestGetRequest(t *testing.T) {
 			})
 		}),
 	)
-
+	// wait for server ready
+	time.Sleep(1 * time.Millisecond)
 	defer server.Stop()
 
 	//2. client request it
@@ -53,8 +56,61 @@ func TestGetRequest(t *testing.T) {
 		t.Errorf("json Unmarshal error  = %v", err)
 	}
 	//3. assert it
-	assert.NotEmpty(t, result.RequestId, "RequestId not found")
+	assert.NotEmpty(t, result.RequestId, "Request Id not found")
 	assert.Equal(t, result.PathParams["id"], "10002")
 	assert.Equal(t, result.QueryParams["from"][0], "10")
 	assert.Equal(t, result.QueryParams["to"][0], "90")
+}
+
+type PostRequest struct {
+	FirstName string `json:"FirstName"`
+	LastName  string `json:"LastName"`
+}
+
+type PostResponse struct {
+	Id       string `json:"id"`
+	FullName string `json:"FullName"`
+}
+
+func TestPostRequestUsingHandlerJson(t *testing.T) {
+	topic := nats.RandomString(4)
+	//1. setup server
+	server := nats.NewServerAndStartRoutine(
+		nats.Subject(topic),
+		nats.Routes(func(r nats.Router) {
+			r.RegisterJson("POST", "/api/test/post/{id}", func(c *nats.Context, rq *PostRequest) (*PostResponse, error) {
+				return &PostResponse{
+					Id:       c.PathParams()["id"],
+					FullName: fmt.Sprintf("%s %s", rq.FirstName, rq.LastName),
+				}, nil
+			})
+		}),
+	)
+
+	// wait for server ready
+	time.Sleep(1 * time.Millisecond)
+	defer server.Stop()
+
+	//2. client request it
+	potsRequest := &PostRequest{FirstName: "", LastName: ""}
+	request, _ := nats.NewReqBuilder().
+		Post("/api/test/post/1111").
+		Subject(topic).
+		BodyJSON(potsRequest).
+		Build()
+
+	msg, err := nats.NewClient(oNats.DefaultURL).Request(request)
+	if err != nil {
+		t.Errorf("Error = %v", err)
+	}
+
+	result := &PostResponse{}
+	err = json.Unmarshal(msg.Body, &result)
+	if err != nil {
+		t.Errorf("json Unmarshal error  = %v", err)
+	}
+
+	//3. assert it
+	assert.NotEmpty(t, result.Id, "Request Id not found")
+	assert.Equal(t, result.FullName, fmt.Sprintf("%s %s", potsRequest.FirstName, potsRequest.LastName))
 }
