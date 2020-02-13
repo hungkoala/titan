5e3149bda11ecddf3c74a9c550375b6a337113b6
Hoa Le <hoa@silentium.io>
2020-02-13 14:21:47 +0700

    nats.go client use a global subscriber to receive and handle reply (INBOX) This subscriber is created upon the first request sent from nats.go client to nats server In unknown situation (e.g. slow build server), that subscriber might not get to the server before the reply sent to nats server, hence the "nats timeout" error happens on nats.go client This commit attemps a hacky fix to send a random request to nats server and flush that global subscriber in case "nats timeout" error happens

--- connection.go
+++ connection.go
@@ -1,9 +1,9 @@
 package titan
 
 import (
-	"github.com/pkg/errors"
-
 	"github.com/nats-io/nats.go"
+	"github.com/pkg/errors"
+	"time"
 )
 
 type Connection struct {
@@ -25,6 +25,11 @@ func NewConnection(url string, options ...nats.Option) (*Connection, error) {
 		return nil, errors.WithMessage(err, "Cannot construct JSON encoded connection to NATS")
 	}
 
+	err = ensureGlobalSubscriber(enc)
+	if err != nil {
+		return nil, errors.WithMessage(err, "Cannot ensure that global subscriber has been created and flushed")
+	}
+
 	return &Connection{Conn: enc}, nil
 }
 
@@ -35,4 +40,19 @@ func (srv *Connection) SendRequest(rq *Request, subject string) (*Response, erro
 	rp := Response{}
 	err := srv.Conn.Request(subject, rq, &rp, GetNatsConfig().GetReadTimeoutDuration())
 	return &rp, err
+}
+
+func ensureGlobalSubscriber(conn *nats.EncodedConn) error {
+	rq, _ := NewReqBuilder().
+		Get("/some/random/non/existent/path/just/to/ensure/the/global/subscriber/to/be/created").
+		Build()
+
+	var rs interface{}
+	err := conn.Request("pleasedonotuse.thisuglyhackystuff.pleasepleaseplease", rq, &rs, 1*time.Millisecond)
+	if err != nil && err.Error() == "nats: timeout" {
+		// expected error, do nothing
+		return conn.Flush()
+	}
+
+	return err
 }
