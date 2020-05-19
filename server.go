a467a1d9a505af3abac89b3fb34293d6f21d2422
hoai nguyen <hoai.nguyen@silentium.io>
2020-05-19 12:25:16 +0700

    Fix return error with json string format to avoid parsing json error on frontend.

--- server.go
+++ server.go
@@ -142,7 +142,7 @@ func (srv *Server) start() error {
 		return errors.New("nats: ReadTimeout can not be empty")
 	}
 
-	timeoutHandler := http.TimeoutHandler(srv.handler, config.GetReadTimeoutDuration(), "nats handler timeout")
+	timeoutHandler := http.TimeoutHandler(srv.handler, config.GetReadTimeoutDuration(), `{"message": "nats handler timeout"}`)
 
 	srv.logger.Info("Connecting to NATS Server at: ", map[string]interface{}{"add": config.Servers})
 	conn, err := NewConnection(
